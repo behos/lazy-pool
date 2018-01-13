40190f19fc38039f3aef787d7653c1bac259fa7e
Giorgos Georgiou <giorgos.georgiou@outlook.com>
2018-01-13 02:13:50 +0200

    Add tests around the expected functionality of the pool

--- src/lib.rs
+++ src/lib.rs
@@ -78,17 +78,68 @@ impl<T> Deref for Pooled<T> {
 #[cfg(test)]
 mod tests {
 
+    extern crate uuid;
+
+    use std::thread;
+    use std::collections::HashSet;
+    use std::time::Duration;
+    use std::sync::{Arc, Mutex};
+
     use super::Pool;
 
     #[derive(PartialEq, Debug)]
     struct AnyObject {
-        member: i32,
+        member: String,
+    }
+
+    impl AnyObject {
+        fn new() -> Self {
+            AnyObject { member: uuid::Uuid::new_v4().to_string() }
+        }
     }
 
     #[test]
     fn can_get_object_from_pool() {
+        let mut pool = Pool::new(10, || AnyObject { member: String::from("member") });
+        assert_eq!(AnyObject { member: String::from("member") }, *pool.get());
+    }
 
-        let mut pool = Pool::new(10, || AnyObject { member: 4 });
-        assert_eq!(AnyObject { member: 4 }, *pool.get());
+    #[test]
+    fn can_get_multiple_objects_from_pool() {
+        let mut pool = Pool::new(10, AnyObject::new);
+        assert!(*pool.get() != *pool.get());
+    }
+
+    #[test]
+    fn can_item_is_relased_back_to_the_start_of_the_pool_when_dropped() {
+        let mut pool = Pool::new(10, AnyObject::new);
+        let member_name_1 = (*pool.get()).member.clone();
+        let member_name_2 = (*pool.get()).member.clone();
+        let member_name_3 = (*pool.get()).member.clone();
+        assert_eq!(member_name_1, member_name_2);
+        assert_eq!(member_name_2, member_name_3);
+    }
+
+    #[test]
+    fn can_share_pool_between_threads() {
+        let pool = Pool::new(10, AnyObject::new);
+        let members = Arc::new(Mutex::new(HashSet::<String>::new()));
+        let mut handles = vec![];
+
+        for _ in 1..10 {
+            let mut local_pool = pool.clone();
+            let local_members = members.clone();
+            let handle = thread::spawn(move || {
+                let pool = local_pool.get();
+                let value = pool.member.clone();
+                local_members.lock().unwrap().insert(value);
+                thread::sleep(Duration::from_millis(100));
+            });
+            handles.push(handle);
+        }
+        for handle in handles {
+            handle.join().unwrap();
+        }
+        assert!(members.lock().unwrap().len() > 5);
     }
 }
