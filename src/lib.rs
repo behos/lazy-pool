487ad3d2ade4da7243a74b311a2f7ca9a98ac808
Giorgos Georgiou <giorgos.georgiou@outlook.com>
2018-02-17 17:52:01 +0200

    Properly implement Future
    Unfortunately I didn't fully understand the requirements of .poll() as
    it seems to be the case with all users of Tokio who want to implement Future.

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,51 @@
-#![feature(box_syntax)]
+#![feature(box_syntax, universal_impl_trait)]
 
+#[macro_use]
+extern crate log;
 extern crate futures;
+
 pub mod errors;
 
 use std::collections::LinkedList;
+use std::fmt::{Debug, Formatter, Error as FmtError};
 use std::ops::Deref;
 use std::sync::{Arc, Mutex};
 
 use futures::{Future, Async, Poll};
-
+use futures::task::{current, Task};
 
 use errors::PoolError;
 
 
-#[derive(Debug)]
 struct SharedPool<T> {
     size: usize,
     created: usize,
     pooled_items: LinkedList<T>,
-    factory: fn() -> T,
+    factory: Box<Fn() -> T + Send + Sync>,
 }
 
+impl<T> Debug for SharedPool<T> {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), FmtError> {
+        write!(
+            fmt,
+            "SharedPool(available: {}, total: {})",
+            self.pooled_items.len(),
+            self.created
+        )
+    }
+}
 
 #[derive(Debug)]
 pub struct Pool<T> {
     shared_pool: Arc<Mutex<SharedPool<T>>>,
+    tasks: Arc<Mutex<LinkedList<Task>>>,
 }
 
 
 impl<T> Pool<T> {
-    pub fn new(size: usize, factory: fn() -> T) -> Self {
+    pub fn new(size: usize, factory: Box<Fn() -> T + Send + Sync>) -> Self {
         Pool {
+            tasks: Arc::new(Mutex::new(LinkedList::new())),
             shared_pool: Arc::new(Mutex::new(SharedPool {
                 size: size,
                 created: 0,
@@ -48,18 +63,26 @@ impl<T> Pool<T> {
     }
 
     fn release(&mut self, item: T) {
-        (self.shared_pool.lock().unwrap()).pooled_items.push_front(
-            item,
-        )
+        let mut shared_pool = self.shared_pool.lock().unwrap();
+        shared_pool.pooled_items.push_front(item);
+        if let Some(task) = self.tasks.lock().unwrap().pop_front() {
+            debug!("Notifying waiting task: {:?}", shared_pool);
+            task.notify()
+        }
+        debug!("Releasing: {:?}", shared_pool);
     }
 
     fn get_if_available(&mut self) -> Option<T> {
         let mut shared_pool = self.shared_pool.lock().unwrap();
         match shared_pool.pooled_items.pop_front() {
-            Some(object) => Some(object),
+            Some(object) => {
+                debug!("Acquiring: {:?}", shared_pool);
+                Some(object)
+            }
             None => {
                 if shared_pool.created < shared_pool.size {
                     shared_pool.created += 1;
+                    debug!("Creating: {:?}", shared_pool);
                     Some((shared_pool.factory)())
                 } else {
                     None
@@ -72,7 +95,10 @@ impl<T> Pool<T> {
 
 impl<T> Clone for Pool<T> {
     fn clone(&self) -> Self {
-        Pool { shared_pool: self.shared_pool.clone() }
+        Pool {
+            shared_pool: self.shared_pool.clone(),
+            tasks: self.tasks.clone(),
+        }
     }
 }
 
@@ -113,18 +139,24 @@ impl<T> Future for FuturePooled<T> {
     type Error = PoolError;
 
     fn poll<'a>(&'a mut self) -> Poll<Self::Item, Self::Error> {
+        debug!("Polling pooled item");
         if self.taken {
             Err(PoolError::PollError)
         } else {
             match self.pool.get_if_available() {
                 Some(object) => {
                     self.taken = true;
+                    debug!("Pooled item ready!");
                     Ok(Async::Ready(Pooled {
                         pool: self.pool.clone(),
                         wrapped: Some(object),
                     }))
                 }
-                None => Ok(Async::NotReady),
+                None => {
+                    self.pool.tasks.lock().unwrap().push_front(current());
+                    debug!("Pooled item not Ready! Enqueueing task");
+                    Ok(Async::NotReady)
+                }
             }
         }
     }
@@ -141,7 +173,7 @@ mod tests {
     use std::time::Duration;
     use std::sync::{Arc, Mutex};
 
-    use futures::{Future, Async};
+    use futures::Future;
 
     use super::Pool;
 
@@ -154,11 +186,15 @@ mod tests {
         fn new() -> Self {
             AnyObject { member: uuid::Uuid::new_v4().to_string() }
         }
+
+        fn with_context(context: &str) -> Self {
+            AnyObject { member: String::from(context) }
+        }
     }
 
     #[test]
     fn can_get_object_from_pool() {
-        let pool = Pool::new(10, || AnyObject { member: String::from("member") });
+        let pool = Pool::new(10, box || AnyObject { member: String::from("member") });
         assert_eq!(
             AnyObject { member: String::from("member") },
             *pool.get().wait().unwrap()
@@ -167,13 +203,13 @@ mod tests {
 
     #[test]
     fn can_get_multiple_objects_from_pool() {
-        let pool = Pool::new(10, AnyObject::new);
+        let pool = Pool::new(10, box AnyObject::new);
         assert!(*pool.get().wait().unwrap() != *pool.get().wait().unwrap());
     }
 
     #[test]
     fn item_is_relased_back_to_the_start_of_the_pool_when_dropped() {
-        let pool = Pool::new(10, AnyObject::new);
+        let pool = Pool::new(10, box AnyObject::new);
         let member_name_1 = (*pool.get().wait().unwrap()).member.clone();
         let member_name_2 = (*pool.get().wait().unwrap()).member.clone();
         let member_name_3 = (*pool.get().wait().unwrap()).member.clone();
@@ -183,7 +219,7 @@ mod tests {
 
     #[test]
     fn can_share_pool_between_threads() {
-        let pool = Pool::new(10, AnyObject::new);
+        let pool = Pool::new(3, box || AnyObject::new());
         let members = Arc::new(Mutex::new(HashSet::<String>::new()));
         let mut handles = vec![];
 
@@ -191,60 +227,26 @@ mod tests {
             let mut local_pool = pool.clone();
             let local_members = members.clone();
             let handle = thread::spawn(move || {
-                let pool = local_pool.get();
-                let value = pool.wait().unwrap().member.clone();
+                let pool = local_pool.get().wait().unwrap();
+                let value = pool.member.clone();
                 local_members.lock().unwrap().insert(value);
-                thread::sleep(Duration::from_millis(500));
+                thread::sleep(Duration::from_millis(200));
             });
             handles.push(handle);
         }
         for handle in handles {
             handle.join().unwrap();
         }
-        assert!(members.lock().unwrap().len() > 3);
+        assert_eq!(members.lock().unwrap().len(), 3);
     }
 
     #[test]
-    fn additional_polls_need_to_wait_for_the_pooled_item_to_be_freed() {
-        let pool = Pool::new(1, AnyObject::new);
-        let mut first = pool.get();
-        let mut second = pool.get();
-
-        {
-            // While the item is in scope, it's busy
-            let item = match first.poll() {
-                Ok(Async::NotReady) => None,
-                Ok(Async::Ready(o)) => Some(o),
-                _ => None,
-            };
-
-            assert!(item.is_some());
-
-            match second.poll() {
-                Ok(Async::Ready(_)) => assert!(false),
-                _ => (),
-            }
-        }
-
-        let item = match second.poll() {
-            Ok(Async::NotReady) => None,
-            Ok(Async::Ready(o)) => Some(o),
-            _ => None,
-        };
-        assert!(item.is_some());
+    fn can_use_closure_as_factory() {
+        let context = "hello";
+        let pool = Pool::new(10, box move || AnyObject::with_context(context));
+        assert_eq!(
+            AnyObject { member: String::from("hello") },
+            *pool.get().wait().unwrap()
+        );
     }
-
-    #[test]
-    fn polls_on_consumed_future_raise_error() {
-        let pool = Pool::new(1, AnyObject::new);
-        let mut first = pool.get();
-        match first.poll() {
-            Ok(Async::NotReady) => None,
-            Ok(Async::Ready(o)) => Some(o),
-            _ => None,
-        };
-        let poll_result = first.poll();
-        assert!(poll_result.is_err());
-    }
-
 }
